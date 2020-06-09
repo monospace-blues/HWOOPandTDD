3398e27af70ff12fade369ea53926ed018c23b6e
Christian Gonzalez <christian.gonzalez.2633@gmail.com>
2020-06-09 15:47:46 -0500

    working app to generate html file

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-// prompt user function
+// array for employees
+let employeeArray = [];
+
+// prompt user for manager
 let promptManager = () => { return inquirer.prompt([
         {
             type: "input",
@@ -31,15 +34,22 @@ let promptManager = () => { return inquirer.prompt([
             type: "input",
             message: "Type in office number: ",
             name: "officeNumber"
+        },
+        {
+            type: "list",
+            message: "Do you want to add more employees?",
+            choices: ["Intern", "Engineer", "No more employees"],
+            name: "choice",
+
         }
     ]);
 }
 
-let promptEmployee = () => {
+let promptIntern = () => {
     return inquirer.prompt([
         {
             type: "input",
-            message: "Name of the mananger?",
+            message: "Name of the Intern?",
             name: "name"
         },
         {
@@ -54,24 +64,140 @@ let promptEmployee = () => {
         },
         {
             type: "input",
-            message: "Type in office number: ",
-            name: "email"
+            message: "Type in school: ",
+            name: "school"
+        },
+        {
+            type: "list",
+            message: "Do you want to add more employees?",
+            choices: ["Intern", "Engineer", "No more employees"],
+            name: "choice",
+
         }
-    ])
+    ]);
 
 }
 
-promptManager().then(answers => {
-    let manager = new Manager(answers.name
-        ,answers.id 
-        ,answers.email 
-        ,answers.officeNumber);
-    console.log(manager);
-})
-// if there is an error, catch and throw it
-.catch(function (err) {
-    console.log(err);
-});
+let promptEngineer = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Name of the Engineer?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "Type in ID number: ",
+            name: "id"
+        },
+        {
+            type: "input",
+            message: "Type in email: ",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "Type in Github name: ",
+            name: "github"
+        },
+        {
+            type: "list",
+            message: "Do you want to add more employees?",
+            choices: ["Intern", "Engineer", "No more employees"],
+            name: "choice",
+
+        }
+    ]);
+
+}
+
+// // testing the html workaround
+// // TEST: WORKS! hallelujah
+// promptManager().then(answers => {
+//     // create manager obj from the inquirer text
+//     let manager = new Manager(answers.name
+//         ,answers.id 
+//         ,answers.email 
+//         ,answers.officeNumber);
+//     console.log(manager);
+
+//     // push to the employee array
+//     employeeArray.push(manager);
+
+//     // retrieve html block and put it into a variable (htmlText)
+//     let htmlText = render(employeeArray);
+
+//     // write to the output folder
+//     return fs.writeFileSync(outputPath, htmlText);
+// })
+// // if there is an error, catch and throw it
+// .catch(function (err) {
+//     console.log(err);
+// });
+
+async function questionsLoop () {
+    // condition to continue the for loop
+    let donewith = false;
+
+    // manager will be asked first, so call promptManager()
+    let res = await promptManager();
+
+    // once we get the info for promptManager(), we'll create/initialize
+    // the Manager obj and push it to our employeeArray
+    let manager = new Manager(res.name,
+        res.id,
+        res.email,
+        res.officeNumber);
+    employeeArray.push(manager);
+    
+    // afterwards, we now check the answer to see whether we'll add more employees to our page
+    //
+    // while we're not done with our loop (!donewith), we'll see if the added employee is either an intern or engineer
+    while (!donewith) {
+
+        // if it is an intern, we create/initialize an Intern obj. and push it to the employeeArray
+        if (res.choice === "Intern") {
+            // we'll continue to use the res variable to retrive the inquirer return objects.
+            res = await promptIntern();
+            // initialize Intern obj.
+            let intern = new Intern(res.name,
+                res.id,
+                res.email,
+                res.school);
+            // push to employeeArray
+            employeeArray.push(intern);
+        }
+        // if it is an engineer, we create/initialize an Engineer obj. and push it to the employeeArray
+        else if (res.choice === "Engineer") {
+            res = await promptEngineer();
+            // initialize Engineer obj.
+            let engineer = new Engineer(res.name,
+                res.id,
+                res.email,
+                res.github);
+            // push to employeeArray
+            employeeArray.push(engineer);
+        }
+        else if (res.choice === "No more employees") {
+            donewith = true;
+            console.log(employeeArray);
+            
+        }
+
+    }
+
+    // once done,
+    // retrieve html block and put it into a variable (htmlText)
+    let htmlText = render(employeeArray);
+
+    // write to the output folder
+    return fs.writeFileSync(outputPath, htmlText);
+
+    
+}
+
+// call the questions loop
+questionsLoop();
 
 
 // Write code to use inquirer to gather information about the development team members,
